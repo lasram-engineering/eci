8be6bbaa87ebf405963aa75084a0650e9bb7068d
Puskapeti <p.puska@lasram.hu>
2023-05-23 13:20:33 +0200

    Implemented IoT Agent attr update

--- main/include/fiware.h
+++ main/include/fiware.h
@@ -2,4 +2,6 @@
 
 #include "esp_err.h"
 
-esp_err_t update_attribute(const char name, const char *value);
+esp_err_t fiware_update_attribute(const char name, const char *value);
+
+esp_err_t fiware_check_health();
