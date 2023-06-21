669c7552e6ffe99804faafd3dab528b4bb2c8825
Puskapeti <p.puska@lasram.hu>
2023-06-21 14:11:21 +0200

    Fully implemented Kawasaki UART comm protocol

--- main/tasks/uart_task.c
+++ main/tasks/uart_task.c
@@ -28,8 +28,21 @@
 
 #define UART_MAX_RESPONSE_LENGTH 32
 
-/** If no payload arrives after this time has elapsed from the ACK send end the transmission */
+/**
+ * If both the robot and the ECI initiaites a transmission, the robot won't ACK the transmission
+ * This is an ENQ collision
+ *
+ * Basically how long we have to wait for an ACK signal
+ */
+#define PROTOCOL_T1 100
+/** The time to wait for an ACK after sending the payload */
+#define PROTOCOL_T2 100
+/** The time to wait for the payload after the ACK signal */
 #define PROTOCOL_T3 1000
+/** The number of retries after not receiving ACK in T2 time*/
+#define PROTOCOL_C1 3
+/** The number of retries after a NAK response */
+#define PROTOCOL_C2 3
 
 // transmission flags from the Kawasaki controller
 /** Enquiry, this means the beginning of a transmission */
@@ -42,6 +55,8 @@ static const char UNICODE_STX = 2;
 static const char UNICODE_ETX = 3;
 /** End of transmission, this closes the transmission */
 static const char UNICODE_EOT = 4;
+/** Not acknowledge */
+static const char UNICODE_NAK = 15;
 
 static char *TAG = "UART";
 static char uart_response[UART_MAX_RESPONSE_LENGTH];
@@ -107,13 +122,89 @@ esp_err_t uart_read_string(uart_port_t port)
 }
 
 /**
+ * Waits for the STX and then receives the payload and waits for the ETX flag
+ * @returns
+ *  ESP_OK on success,
+ *  ESP_FAIL if there was an error,
+ *  ESP_ERR_INVALID_RESPONSE if the response was invalid,
+ *  ESP_ERR_TIMEOUT if there was a timeout
+ */
+esp_err_t uart_read_transmission_payload(uart_port_t port)
+{
+    esp_err_t ret;
+    char input;
+
+    while (1)
+    {
+        // wait for the ACK (or EOT)
+        ret = uart_read_bytes(port, &input, 1, PROTOCOL_T3 / portTICK_PERIOD_MS);
+
+        // check for timeout
+        if (ret == 0)
+        {
+            // send abnormal EOT back
+            uart_write_bytes(port, &UNICODE_EOT, 1);
+            return ESP_ERR_TIMEOUT;
+        }
+
+        // abnormal EOT (likely due to too many retries)
+        if (input == UNICODE_EOT)
+            return ESP_FAIL;
+
+        // if the token is not STX it is an invalid response
+        else if (input != UNICODE_STX)
+            return ESP_ERR_INVALID_RESPONSE;
+
+        // normal operation, receive the payload
+        int c;
+        for (c = 0; c < UART_MAX_RESPONSE_LENGTH; c++)
+        {
+            // read byte by byte
+            ret = uart_read_bytes(port, &input, 1, PROTOCOL_T3 / portTICK_PERIOD_MS);
+
+            // check for timeout
+            if (ret == 0)
+            {
+                // terminate abnormally
+                uart_write_bytes(port, &UNICODE_EOT, 1);
+                return ESP_ERR_TIMEOUT;
+            }
+
+            // check if the text is ended
+            if (input == UNICODE_ETX)
+            {
+                // terminate the string
+                uart_response[c] = '\0';
+                return ESP_OK;
+            }
+            else
+            {
+                // append the input to the string
+                uart_response[c] = input;
+            }
+        }
+
+        // if program reaches here it means that ETX was never received
+        // and the max uart response length was reached
+        // send a NACK to the controller
+        uart_write_bytes(port, &UNICODE_NAK, 1);
+        ESP_LOGW(TAG, "Response too long, sending back NACK and retrying...");
+    }
+}
+
+/**
  * Waits until an incoming transmission from the robot controller
  * When the transmission comes in, decodes it and puts it in the response string
+ * @returns
+ *  ESP_OK if successful,
+ *  ESP_FAIL if there was an error,
+ *  ESP_TIMEOUT if there was a timeout
+ *  ESP_ERR_INVALID_RESPONSE if the response was invalid
  */
 esp_err_t uart_read_transmission(uart_port_t port)
 {
     char input;
-    int ret;
+    esp_err_t ret;
 
     while (input != UNICODE_ENQ)
     {
@@ -121,45 +212,18 @@ esp_err_t uart_read_transmission(uart_port_t port)
         // hangs indefinitely
         ret = uart_read_bytes(port, &input, 1, portMAX_DELAY);
 
-        if (ret <= 0)
+        if (ret == 0)
             return ESP_ERR_TIMEOUT;
     }
 
     // after the ENQ is received, send an ACK
     uart_write_bytes(port, &UNICODE_ACK, 1);
 
-    // after the ACK was sent, the controller will send a STX
-    ret = uart_read_bytes(port, &input, 1, PROTOCOL_T3 / portTICK_PERIOD_MS);
+    // read the transmission payload
+    ret = uart_read_transmission_payload(port);
 
-    if (ret <= 0 || input != UNICODE_STX)
-    {
-        // send abnormal EOT back
-        uart_write_bytes(port, &UNICODE_EOT, 1);
-        return ESP_ERR_INVALID_RESPONSE;
-    }
-    // after this, read until the ETX flag is sent
-    int i = 0;
-    for (i = 0; i < UART_MAX_RESPONSE_LENGTH; i++)
-    {
-        // read byte by byte
-        ret = uart_read_bytes(port, &input, 1, portMAX_DELAY);
-
-        if (ret <= 0)
-            return ESP_ERR_TIMEOUT;
-
-        if (input != UNICODE_ETX)
-        {
-            uart_response[i] = input;
-        }
-        else
-        {
-            // the payload has ended
-            // terminate the string
-            uart_response[i] = '\0';
-
-            break;
-        }
-    }
+    if (ret != ESP_OK)
+        return ret;
 
     // send back the ACK signal
     uart_write_bytes(port, &UNICODE_ACK, 1);
@@ -168,6 +232,82 @@ esp_err_t uart_read_transmission(uart_port_t port)
     uart_read_bytes(port, &input, 1, portMAX_DELAY);
 
     return ESP_OK;
+}
+
+/**
+ * Sends a transmission with the given payload to the robot controller
+ * @returns
+ *  ESP_OK if success,
+ *  ESP_INVALID_RESPONSE if a response was invalid
+ *  ESP_ERR_NOT_FINISHED if there was an ENQ collision (check uart_response)
+ *  ESP_FAIL if the transmission has failed
+ */
+esp_err_t uart_write_transmission(uart_port_t port, const char *payload)
+{
+    esp_err_t ret;
+    char input;
+    int retry;
+    bool skip_enq = false;
+
+    // No answer means retry for at max C1 times
+    for (retry = 1; retry <= PROTOCOL_C1; retry++)
+    {
+        if (!skip_enq)
+        {
+            // initiate the transmission with the ENQ token
+            uart_write_bytes(port, &UNICODE_ENQ, 1);
+
+            // wait for the ACK token
+            ret = uart_read_bytes(port, &input, 1, PROTOCOL_T1 / portTICK_PERIOD_MS);
+
+            // ENQ collision
+            // this can happen if there is a timeout or the input is an ENQ
+            if (ret == 0 || input == UNICODE_ENQ)
+            {
+                return ESP_ERR_NOT_FINISHED;
+            }
+        }
+
+        // normal operation
+        // send the STX token
+        uart_write_bytes(port, &UNICODE_STX, 1);
+        // send the payload
+        uart_write_bytes(port, payload, strlen(payload));
+        // send the ETX token
+        uart_write_bytes(port, &UNICODE_ETX, 1);
+
+        // wait for the ACK/NACK for T2
+        ret = uart_read_bytes(port, &input, 1, PROTOCOL_T2 / portTICK_PERIOD_MS);
+
+        // if no response, retry
+        if (ret <= 0)
+            continue;
+
+        // response is either ACK or NACK
+        if (input == UNICODE_NAK)
+        {
+            // retry sending the payload
+            skip_enq = true;
+            continue;
+        }
+        else if (input == UNICODE_ACK)
+        {
+            // terminate normally
+            uart_write_bytes(port, &UNICODE_EOT, 1);
+            return ESP_OK;
+        }
+        else
+        {
+            // response is invalid
+            return ESP_ERR_INVALID_RESPONSE;
+        }
+    }
+
+    // if we reach this point there were too many retries and no answer
+    // terminate abnormally
+    uart_write_bytes(port, &UNICODE_EOT, 1);
+
+    return ESP_FAIL;
 }
 
 void uart_task(void *arg)
@@ -216,11 +356,25 @@ void uart_task(void *arg)
         // wait for the transmission
         ret = uart_read_transmission(uart_robot);
 
-        if (ret == ESP_ERR_TIMEOUT)
-            ESP_LOGE(TAG, "Read timed out");
-        else if (ret == ESP_ERR_INVALID_RESPONSE)
-            ESP_LOGE(TAG, "Invalid response");
-        else
-            ESP_LOGI(TAG, "Incoming transmission: %s", uart_response);
+        switch (ret)
+        {
+        case ESP_FAIL:
+            ESP_LOGW(TAG, "Incoming transmission read failed");
+            break;
+
+        case ESP_ERR_TIMEOUT:
+            ESP_LOGW(TAG, "Incoming transmission has timed out");
+            break;
+
+        case ESP_ERR_INVALID_RESPONSE:
+            ESP_LOGW(TAG, "Incoming transmission had invalid response");
+            break;
+
+        case ESP_OK:
+            ESP_LOGI(TAG, "Incoming transmission %s", uart_response);
+
+        default:
+            break;
+        }
     }
 }
