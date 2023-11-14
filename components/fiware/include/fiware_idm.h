a5bcd66a1620283d325850a926e224e49887566d
ppuska <p.puska@lasram.hu>
2023-11-14 11:00:50 +0100

    Implemented FIWARE IdM security protocols

---
+++ components/fiware/include/fiware_idm.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <esp_err.h>
+
+#include <stdbool.h>
+#include <time.h>
+
+#define FIWARE_IDM_HEADER_AUTH_TOKEN "X-Auth-Token"
+
+#define FIWARE_IDM_TOKEN_LEN 40
+#define FIWARE_AUTH_HEADER_BASIC_LEN 6     // length of 'Basic '
+#define FIWARE_AUTH_BASE64_ENCODED_LEN 100 // 36 byte id + ':' + 36 byte secret
+#define FIWARE_AUTH_BUF_LEN FIWARE_AUTH_HEADER_BASIC_LEN + FIWARE_AUTH_BASE64_ENCODED_LEN
+
+typedef struct
+{
+    char token[FIWARE_IDM_TOKEN_LEN];
+    char refresh_token[FIWARE_IDM_TOKEN_LEN];
+    time_t expires_in;
+} FiwareAccessToken_t;
+
+typedef enum
+{
+    PASSWORD,
+    REFRESH,
+} FiwareAccessTokenGrantType;
+
+esp_err_t fiware_idm_request_access_token(FiwareAccessToken_t *token);
+
+esp_err_t fiware_idm_renew_access_token(FiwareAccessToken_t *token);
+
+esp_err_t fiware_idm_attach_auth_data_to_request(FiwareAccessToken_t *token, esp_http_client_t client);
+
+bool fiware_idm_check_is_token_expired(FiwareAccessToken_t *token);
