36302e4f32484d07b9b11fd88b28e338cebc28b2
ppuska <p.puska@lasram.hu>
2023-11-14 11:01:32 +0100

    Implemented security protocols in fiware task

--- components/fiware/fiware_task.c
+++ components/fiware/fiware_task.c
@@ -3,9 +3,14 @@
 #include <string.h>
 
 #include <esp_log.h>
+#include <esp_check.h>
+#include <esp_netif_sntp.h>
 
 #include "iot_agent.h"
+#include "fiware_idm.h"
+
 #include "task_intercom.h"
+#include "app_state.h"
 
 static const char *TAG = "FIWARE Task";
 
@@ -16,9 +21,26 @@ static fiware_iota_command_t fiware_incoming_command;
 
 static itc_uart_message_t uart_message;
 
+static FiwareAccessToken_t fiware_access_token;
+
 void fiware_task()
 {
     int ret;
+
+    // wait for wifi to be connected
+    app_state_wait_for_event(STATE_TYPE_INTERNAL, APP_STATE_INTERNAL_WIFI_CONNECTED);
+
+    // wait for network time to be synchronized
+    ESP_ERROR_CHECK_WITHOUT_ABORT(esp_netif_sntp_sync_wait(portMAX_DELAY));
+
+    ESP_LOGI(TAG, "Requesting access token from IdM");
+
+    ret = fiware_idm_request_access_token(&fiware_access_token);
+
+    if (ret == ESP_OK)
+        ESP_LOGI(TAG, "Got access token: %s", fiware_access_token.token);
+    else
+        ESP_LOGE(TAG, "Unable to get access token");
 
     /* LOOP */
     while (1)
@@ -45,7 +67,7 @@ void fiware_task()
             ESP_LOGI(TAG, "New porgam: %s", fiware_incoming_command.command_param);
 
             // load the program name into the message payload
-            sprintf(uart_message.payload, "PROGRAM|%s", fiware_incoming_command.command_param);
+            snprintf(uart_message.payload, CONFIG_ITC_IOTA_MEASUREMENT_MESSAGE_SIZE, "PROGRAM|%s", fiware_incoming_command.command_param);
 
             // send the message to the queue
             ret = xQueueSend(task_intercom_uart_queue, &uart_message, 0);
